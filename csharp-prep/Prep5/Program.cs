09610f490a48c3ede3ad7c4a25568c4fcd0d00eb
paulajessica <u811780803@byui.edu>
2022-09-23 16:20:30 -0300

    Add w02

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -8,6 +8,46 @@ namespace Prep5
         static void Main(string[] args)
         {
             Console.WriteLine("This is Prep 5");
+            DisplayMessage();
+            string username = PromptUserName();
+            int usernumber = PromptUserNumber();
+            int squarenumber = SquareNumber(usernumber);
+            DisplayResult(username, squarenumber);
+
         }
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+
+            Console.Write("What is your name?");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number?");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+        static int SquareNumber(int PromptUserNumber)
+        {
+            int squaredresult = PromptUserNumber * PromptUserNumber;
+            return squaredresult;
+
+        }
+        static void DisplayResult(string name, int number)
+        {
+            Console.WriteLine($"{name}, the square of your number is {number}.");
+        }
+
+
+
+
     }
 }
--- developer/Unit01/Program.cs
+++ developer/Unit01/Program.cs
@@ -1,12 +1,154 @@
 ﻿using System;
+using System.Collections.Generic;
 
+
+// Paula Jessica Ferreira Lucas da Silva
+//W02 Prove: Developer
 namespace Unit01
 {
-    class Program
+    class TicTacToeGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Welcome to Tic-Tac-Toe Game!");
+            Console.WriteLine();
+            // making the game table with index variable like a counter
+            string[,] table = new string[3, 3];
+            int index = 1;
+
+            for (int i = 0; i < table.GetLength(0); i++)
+            {
+                for (int j = 0; j < table.GetLength(1); j++)
+                {
+                    table[i, j] = index.ToString();
+                    index++;
+                }
+            }
+
+            //print the game table
+            for (int i = 0; i < table.GetLength(0); i++)
+            {
+                for (int j = 0; j < table.GetLength(1); j++)
+                {
+                    Console.Write($"{table[i, j]}  |  ");
+                }
+                Console.WriteLine();
+            }
+
+
+            //sking the number from user and starting with letter 'X' in variable turn
+            string turn = "X";
+
+            // List to cheking if user select one played variable valid
+            List<string> numberplayed = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+            //Taking position in table
+            Console.Write($"{turn} turn to choose a square (1-9):");
+            string played = Console.ReadLine();
+
+            //cheking input from user to validation
+
+            string positionselected = PlayedValidation(numberplayed, played, turn);
+
+            //the loop to run Tic-Tac-Toe Game with numbermoves from one to nine
+            int numbermoves = 1;
+            while (numbermoves <= 9)
+            {
+
+
+                //scrolling trhough the table, cheking played from user is equal position in table
+                // if yes then replace number selected for turn(letter X or O)
+                for (int i = 0; i < table.GetLength(0); i++)
+                {
+                    for (int j = 0; j < table.GetLength(1); j++)
+                    {
+                        if (table[i, j] == (positionselected) && numberplayed.Contains(positionselected))
+                        {
+                            table[i, j] = turn;
+                            numberplayed.Remove(positionselected);
+                        }
+                    }
+                }
+                //cleaning old table to after that it prints new table with the replacements
+                Console.Clear();
+
+                Console.WriteLine("Welcome to Tic-Tac-Toe Game!");
+                Console.WriteLine();
+
+                //printing the new table
+                for (int i = 0; i < table.GetLength(0); i++)
+                {
+                    for (int j = 0; j < table.GetLength(1); j++)
+                    {
+                        Console.Write($"{table[i, j]}  |  ");
+                    }
+                    Console.WriteLine();
+                }
+
+                //the counter to increase the numbermoves variable and keeping the loop until nine times
+                numbermoves++;
+
+                //cheking if the user wins the game. If yes, the game finishes.
+                if (table[0, 0] == table[0, 1] && table[0, 0] == table[0, 2] ||
+                    table[1, 0] == table[1, 1] && table[1, 0] == table[1, 2] ||
+                    table[2, 0] == table[2, 1] && table[2, 0] == table[2, 2] ||
+                    table[0, 0] == table[1, 0] && table[0, 0] == table[2, 0] ||
+                    table[0, 1] == table[1, 1] && table[0, 1] == table[2, 1] ||
+                    table[0, 2] == table[1, 2] && table[0, 2] == table[2, 2] ||
+                    table[0, 0] == table[1, 1] && table[0, 0] == table[1, 2] ||
+                    table[0, 2] == table[1, 1] && table[0, 2] == table[2, 0])
+                {
+                    Console.WriteLine($"{turn} win this game. Congrats!!!");
+                    break;
+                }
+                else if (numbermoves == 10)
+                {
+                    Console.WriteLine($"Tied the game!!!");
+                    Console.WriteLine();
+                    break;
+                }
+
+                //changing letter of user
+                //printing and taking new turn variable                
+                turn = ChangeTurn(turn);
+
+                Console.WriteLine();
+                Console.Write($"{turn} turn to choose a square (1-9):");
+                positionselected = Console.ReadLine();
+
+                //cheking input from user to validation                
+                positionselected = PlayedValidation(numberplayed, positionselected, turn);
+                Console.WriteLine();
+
+
+
+            }
+
         }
+
+        static string PlayedValidation(List<string> list, string position, string letter)
+        {
+            while (!list.Contains(position))
+            {
+                Console.Write($"{letter} Option Invalida. Try againd and to choose a square (1-9) again:");
+                position = Console.ReadLine();
+
+            }
+            return position;
+        }
+        static string ChangeTurn(string letter)
+        {
+            if (letter == "X")
+            {
+                letter = "O";
+            }
+            else
+            {
+                letter = "X";
+            }
+            return letter;
+        }
+
     }
 }
