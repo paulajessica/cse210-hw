6516e4d0b603975753eed7897453c5f5a1e4644f
paulajessica <u811780803@byui.edu>
2022-10-16 17:52:20 -0300

    Add w04

---
+++ developer/Unit02/Game/Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Unit02.Game
+{
+    public class Card
+    {
+        public int _value;
+
+        /// <summary>
+        /// Constructs a new instance of Card.
+        /// </summary>
+        public Card()
+        {
+            _value = 0;
+        }
+
+        /// <summary>
+        /// Generates a new random value for the Card.
+        /// </summary>
+        public int Draw()
+        {
+            Random number = new Random();
+            _value = number.Next(1, 14);
+            return _value;
+        }
+    }
+}
+
+
